2a30a14c0f849577409364e7a05df8e76cf18f7f
ayuspark <lola.luyaowang@gmail.com>
2017-10-20 10:45:02 -0700

    added tests and cleared assembly dups

---
+++ gameTest/Class1.cs
@@ -0,0 +1,52 @@
+﻿using Xunit;
+using terminal_rpg;
+
+namespace gameTest
+{
+    public class Class1
+    {
+        Human testplayer = new Human("Test");
+        Wizard testWizard = new Wizard("Test");
+        [Fact]
+        public void HumanActionTest()
+        {
+            Assert.Equal("Attack", testplayer.Actions[1]);
+        }
+
+        [Fact]
+        public void HumanInventoryTest()
+        {
+            Assert.Equal(3, testplayer.Inventory["Heal_Potion"]);
+        }
+
+        [Fact]
+        public void HumanNameTest()
+        {
+            Assert.Equal("Test", testplayer.Name);
+        }
+
+        [Fact]
+        public void AttackTest()
+        {
+            Assert.Equal(100, testplayer.Health);
+            Assert.Equal(50, testWizard.Health);
+            testplayer.Attack(testWizard);
+            Assert.Equal(35, testWizard.Health);
+        }
+
+        [Fact]
+        public void WizardHealTest()
+        {
+            testWizard.Heal();
+            Assert.Equal(175, testWizard.Health);
+        }
+
+        [Fact]
+        //supposed to fail, health damage is random
+        public void WizardFireballTest()
+        {
+            testWizard.Fireball(testplayer);
+            Assert.Equal(2, testplayer.Health);
+        }
+    }
+}
