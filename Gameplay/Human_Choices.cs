e191bcbbeeace72f93a5d523839acb2f4dbf69ac
makiroggers <makiroggers@gmail.com>
2017-10-19 22:57:11 -0700

    I NEED SLEEP

--- Gameplay/Human_Choices.cs
+++ Gameplay/Human_Choices.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace terminal_rpg
 {
@@ -7,7 +8,41 @@ namespace terminal_rpg
         public Human_Choices(string charName)
         {
             Human player = new Human(charName);
+            Goblin enemy = new Goblin("Goblix");
+            System.Console.WriteLine("\nHere are your stats...\n");
             System.Console.WriteLine("Current Player: {0}", player.ShowStatus());
+            Human_Choices.Choose_Action(player);
+        }
+
+        public static void Choose_Action(Human player)
+        {
+            int numSelect;
+            System.Console.WriteLine("\nOh no! A big, evil, scary-looking thing appeared...");
+            // Get user input for selection
+            System.Console.WriteLine(Program.newLine);
+            System.Console.WriteLine("Now you must select a number to choose what you would like to do next.");
+            System.Console.WriteLine(Program.newLine);
+            foreach (KeyValuePair<int, string> action in player.Actions)
+            {
+                System.Console.WriteLine("{0}. {1}\n", action.Key, action.Value);
+            }
+            numSelect = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine(Program.newLine);
+
+            switch (numSelect)
+            {
+                case 1:
+                    System.Console.WriteLine("You chose {0}!", player.Actions[1]);
+                    break;
+                case 2:
+                    System.Console.WriteLine("You chose {0}!", player.Actions[2]);
+                    break;
+                default:
+                    System.Console.WriteLine("Learn how to number!");
+                    System.Console.WriteLine(Program.newLine);
+                    Human_Choices.Choose_Action(player);
+                    break;
+            }
         }
     }
 }
