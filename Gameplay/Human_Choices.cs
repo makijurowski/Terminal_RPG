1f9daa72b5af27fcf971e78e08e7976e6cd8d92d
makiroggers <makiroggers@gmail.com>
2017-10-19 23:50:41 -0700

    ASCII art! :)

--- Gameplay/Human_Choices.cs
+++ Gameplay/Human_Choices.cs
@@ -8,8 +8,10 @@ namespace terminal_rpg
         public Human_Choices(string charName)
         {
             Human player = new Human(charName);
-            System.Console.WriteLine("\nHere are your stats...\n");
+            System.Console.WriteLine("\nHere are your starting stats...\n");
+            System.Console.ReadKey();
             System.Console.WriteLine("Current Player: {0}", player.ShowStatus());
+            System.Console.ReadKey();
             Human_Choices.Choose_Action(player);
         }
 
@@ -17,7 +19,8 @@ namespace terminal_rpg
         {
             int numSelect;
             Goblin enemy = new Goblin("Goblix");
-            System.Console.WriteLine("\nOh no! A big, evil, scary-looking thing appeared...");
+            System.Console.WriteLine("\nOh no! A big, ugly, scary-looking thing appeared...");
+            System.Console.ReadKey();
             // Get user input for selection
             System.Console.WriteLine(Program.newLine);
             System.Console.WriteLine("Now you must select a number to choose what you would like to do next.");
@@ -34,17 +37,20 @@ namespace terminal_rpg
                 case 1:
                     System.Console.WriteLine("You chose {0} on {1} the {2}!", player.Actions[1], enemy.Name, enemy.ClassName);
                     player.Attack(enemy);
-                    System.Console.WriteLine(Program.newLine);
+                    System.Console.ReadKey();
                     System.Console.WriteLine("Your current stats: {0}\n", player.ShowStatus());
+                    System.Console.ReadKey();
                     System.Console.WriteLine("Enemy current stats: {0}\n", enemy.ShowStatus());
                     break;
                 case 2:
                     System.Console.WriteLine("You chose {0}!", player.Actions[2]);
+                    System.Console.ReadKey();
                     System.Console.WriteLine(Program.newLine);
                     System.Console.WriteLine("Your current stats: {0}\n", player.ShowStatus());
                     break;
                 default:
                     System.Console.WriteLine("Learn how to number!");
+                    System.Console.ReadKey();
                     System.Console.WriteLine(Program.newLine);
                     Human_Choices.Choose_Action(player);
                     break;
--- Program.cs
+++ Program.cs
@@ -14,22 +14,27 @@ namespace terminal_rpg
             string charName;
 
             // Script for starting the game
-            System.Console.WriteLine("First things first. Let's get acquainted!");
+            System.Console.WriteLine("\nFirst things first. Let's get acquainted!");
+            System.Console.ReadKey();
             System.Console.WriteLine(newLine);
             System.Console.WriteLine("What's your favorite color?");
             System.Console.WriteLine(newLine);
             // Intentionally does nothing
             Console.ReadLine();
             System.Console.WriteLine(newLine);
-            System.Console.WriteLine("Hmm... now that's a choice, isn't it? Maybe we should try an easier one.\n\nWhat's your name?");
+            System.Console.WriteLine("Hmm... now that's a, uh, choice, isn't it? Maybe we should try an easier one.\n\n");
+            System.Console.ReadKey();
+            System.Console.WriteLine("What's your name?");
             System.Console.WriteLine(newLine);
             // User input for name
             charName = Console.ReadLine();
             System.Console.WriteLine(newLine);
             System.Console.WriteLine("Great job {0}, I knew you could do it!", charName);
             System.Console.WriteLine(newLine);
+            System.Console.ReadKey();
             System.Console.WriteLine("Now, please enter a number to choose which character you would like to play.");
             System.Console.WriteLine(newLine);
+            System.Console.ReadKey();
             System.Console.WriteLine("1. Human");
             System.Console.WriteLine(newLine);
             System.Console.WriteLine("2. Wizard");
@@ -57,22 +62,27 @@ namespace terminal_rpg
                 {
                     case 1:
                         System.Console.WriteLine("You chose human!");
+                        System.Console.ReadKey();
                         Human_Choices humanPlayer = new Human_Choices(charName);
                         break;
                     case 2:
                         System.Console.WriteLine("You chose wizard!");
+                        System.Console.ReadKey();
                         Wizard_Choices wizardPlayer = new Wizard_Choices(charName);
                         break;
                     case 3:
                         System.Console.WriteLine("You chose samurai!");
+                        System.Console.ReadKey();
                         Samurai_Choices samuraiPlayer = new Samurai_Choices(charName);
                         break;
                     case 4:
                         System.Console.WriteLine("You chose ninja!");
+                        System.Console.ReadKey();
                         Ninja_Choices ninjaPlayer = new Ninja_Choices(charName);
                         break;
                     default:
                         System.Console.WriteLine("You entered an invalid choice. Please try again.");
+                        System.Console.ReadKey();
                         numSelect = Convert.ToInt32(Console.ReadLine());
                         System.Console.WriteLine(Program.newLine);
                         MakeChar player = new MakeChar(numSelect, charName);
@@ -84,12 +94,44 @@ namespace terminal_rpg
         // Main method invoked upon running program
         static void Main(string[] args)
         {
+            var nameArr = new[]
+            {@"                                                                                  ",
+             @"                                                                                  ",
+             @"                                                                                  ",
+             @"   _____          _                                                               ",
+             @"  |  __ \        (_)                                                              ",
+             @"  | |  | |  ___   _   ___                                                         ",
+             @"  | |  | | / _ \ | | / _ \                                                        ",                        
+             @"  | |__| || (_) || || (_) |                                                       ",
+             @"  |_____/  \___/ | | \___/   _                     _                              ",
+             @"                _/ /\       | |                   | |                             ",
+             @"               |__/  \    __| |__   __ ___  _ __  | |_  _   _  _ __  ___  ___     ",
+             @"                 / /\ \  / _` |\ \ / // _ \| '_ \ | __|| | | || '__|/ _ \/ __|    ",
+             @"                / ____ \| (_| | \ V /|  __/| | | || |_ | |_| || |  |  __/\__ \    ",
+             @"               /_/    \_\\__,_|  \_/  \___||_| |_| \__| \__,_||_|   \___||___/    ",
+             @"                                                                                  ",
+             @"                                                                                  ",
+             @"                                                                                  ",
+             @"                                                                                  ",
+             @"           Press any key to begin the best two minutes of your life.              ",
+             @"                                                                                  ",
+             @"                                                                                  ",
+             @"                                                                                  ",
+            };
+            // System.Console.WindowWidth = 160;
+            System.Console.WriteLine("\n\n");
+            foreach (string line in nameArr)
+            {
+                System.Console.WriteLine(line);
+            }
+            System.Console.ReadKey();
             // Greeting for player
             System.Console.WriteLine(newLine);
             System.Console.WriteLine("Welcome to Dojo Adventures!");
+            System.Console.ReadKey();
             System.Console.WriteLine(newLine);
-            System.Console.WriteLine("Are you ready to play Dojo Adventures? Get hyped...");
-            System.Console.WriteLine(newLine);
+            System.Console.WriteLine("Are you ready to play Dojo Adventures? Get your hype pants on...\n");
+            System.Console.ReadKey();
 
             // Initiate a new Game
             Program New_Game = new Program();
